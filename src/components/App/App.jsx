4e56a493dfbc80abf7af6cc98f02a36ea4d863ae
German Sokolovskyy <101786762+karabas11@users.noreply.github.com>
2024-01-29 18:53:23 +0200

    Fix App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
-import ContactForm from '../../ContactForm/ContactForm';
-import ContactList from '../../ContactList/ContactList';
-import Filter from '../../Filter/Filter';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import Filter from '../Filter/Filter';
 import { Container } from './App.styled';
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
 import './index.css';
-import App from 'components/Contact/App/App';
+import App from 'components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
